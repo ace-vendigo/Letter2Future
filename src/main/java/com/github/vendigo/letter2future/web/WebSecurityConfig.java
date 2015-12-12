2b07e329a2fd3d921af4f0171e39aa87c80ee614
vendigo <ace.vendigo@gmail.com>
2015-12-12 19:07:24 +0200

    Fixed security configuration

--- src/main/java/com/github/vendigo/letter2future/web/WebSecurityConfig.java
+++ src/main/java/com/github/vendigo/letter2future/web/WebSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.github.vendigo.letter2future.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
