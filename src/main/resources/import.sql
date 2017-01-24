66ad436c70efc092aa197ae68b08b60a6fb8f9d6
Roman Karpovych <roman.karpovich@softheme.com>
2017-01-24 13:47:45 +0200

    Add missing npm dependancy, fix import.sql for linux

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO USER (USERNAME, PASSWORD, EMAIL, ACTIVE, ROLE) VALUES ('vendigo', 'pass', 'l2f.you.from.future@gmail.com', 1, 'ROLE_USER')
+INSERT INTO user (USERNAME, PASSWORD, EMAIL, ACTIVE, ROLE) VALUES ('vendigo', 'pass', 'l2f.you.from.future@gmail.com', 1, 'ROLE_USER')
